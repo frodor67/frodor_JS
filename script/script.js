9b9d86f853f45eaecf943a640a822b62d42057a0
Sergey Fedoseenkov <frodor@smc-sm.ru>
2021-01-29 11:32:12 +0300

    Урок №2, основное задание

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
-let money;
-let income;
-let addExpeses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let income = 'Фриланс';
+let addExpeses = 'Интернет, такси, коммуналка';
+let deposit = true;
+let mission = 3000000;
+let period = 12;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpeses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' юаней');
 
 
-alert('Изучаю JS');
+addExpeses = addExpeses.toLowerCase();
+console.log(addExpeses.split(', '));
 
-console.log('Продлжаю изучать JS');
+let budgetDay = money / 30;
+
+console.log(budgetDay);
 
